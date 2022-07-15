5172b04475b974e4b95a7e5faf32f4b2a246daa2
curban2336 <curban841@gmail.com>
2022-07-15 19:50:16 -0400

    Canvas establishment and script creation

---
+++ Project/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    //Serialized
+    //canvas
+    [SerializeField] GameObject cDefault;
+    [SerializeField] GameObject cDice;
+    [SerializeField] GameObject cLoot;
+    //action blocks for roll results
+    [SerializeField] GameObject block;
+    [SerializeField] GameObject attack;
+    [SerializeField] GameObject ability;
+    //text for action roll results
+    [SerializeField] GameObject tBlock;
+    [SerializeField] GameObject tAttack;
+    [SerializeField] GameObject tAbility;
+    //
+    [SerializeField]
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
