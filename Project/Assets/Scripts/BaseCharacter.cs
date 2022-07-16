ee1d21850f85e0aa0b1dd4ee7970e5a9470844aa
patd0herty <70355923+patd0herty@users.noreply.github.com>
2022-07-16 00:07:39 -0400

    Finished the Enemy Script
    Need to add Player class to the Enemy, and any Animation Code

--- Project/Assets/Scripts/BaseCharacter.cs
+++ Project/Assets/Scripts/BaseCharacter.cs
@@ -11,7 +11,7 @@ public class BaseCharacter : MonoBehaviour
     public int Health { get { return health; } }
     public int Block { get { return block; } }
     public int Attack { get { return attack; } }
-    public string Name { get { return name; } }
+    public string Name { get { return title; } }
 
     // Protected
     protected int health;
@@ -20,7 +20,7 @@ public class BaseCharacter : MonoBehaviour
     protected int totalD6s;
     protected int totalD8s;
     protected int totalD12s;
-    protected string name;
+    protected string title;
 
     // Start is called before the first frame update
     void Start()
