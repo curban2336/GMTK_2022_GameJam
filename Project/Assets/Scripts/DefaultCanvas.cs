25ea64798f4e3bb7bc94fb005400f24fbc13be47
curban2336 <curban841@gmail.com>
2022-07-16 14:28:21 -0400

    More updates for canvas UI scripts

--- Project/Assets/Scripts/DefaultCanvas.cs
+++ Project/Assets/Scripts/DefaultCanvas.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefaultCanvas : MonoBehaviour
 {
@@ -21,6 +22,16 @@ public class DefaultCanvas : MonoBehaviour
     [SerializeField] GameObject attackText;
     [SerializeField] GameObject ability;
     [SerializeField] GameObject abilityText;
+
+    //bools to help handle rotations
+    private bool sixRoll = false;
+    private bool eightRoll = false;
+    private bool twelveRoll = false;
+
+    //counts for the total roll results per action
+    public int blockCount = 0;
+    public int attackCount = 0;
+    public int abilityCount = 0;
 
     //reference to canvas manager
     [SerializeField] GameObject manager;
@@ -67,12 +78,97 @@ public class DefaultCanvas : MonoBehaviour
         aDice.SetActive(false);
         eTurn.SetActive(false);
 
+        dSixText.GetComponent<Text>().text = " ";
+        dEightText.GetComponent<Text>().text = " ";
+        dTwelveText.GetComponent<Text>().text = " ";
 
+        StartCoroutine(Results());
+    }
+
+    private IEnumerator Results()
+    {
+        int sixCount = 0;
+        int eightCount = 0;
+        int twelveCount = 0;
+
+        int rollNum = 0;
+
+        //block.GetComponent<Halo>().enabled = true;
+
+        sixCount = manager.GetComponent<CanvasManager>().sixTallyB;
+        eightCount = manager.GetComponent<CanvasManager>().eightTallyB;
+        twelveCount = manager.GetComponent<CanvasManager>().twelveTallyB;
+
+        yield return new WaitForSeconds(0.2f);
+
+        for(int i = 0; i < sixCount; i++)
+        {
+            rollNum = player.GetComponent<Player>().RollD6s();
+            blockCount += rollNum;
+            sixRoll = true;
+            yield return new WaitForSeconds(1f);
+            sixRoll = false;
+            dSixText.GetComponent<Text>().text = $"{rollNum}";
+            yield return new WaitForSeconds(0.3f);
+            blockText.GetComponent<Text>().text = $"Block: {blockCount}";
+        }
+
+        for (int i = 0; i < eightCount; i++)
+        {
+            rollNum = player.GetComponent<Player>().RollD8s();
+            blockCount += rollNum;
+            eightRoll = true;
+            yield return new WaitForSeconds(1f);
+            eightRoll = false;
+            dEightText.GetComponent<Text>().text = $"{rollNum}";
+            yield return new WaitForSeconds(0.3f);
+            blockText.GetComponent<Text>().text = $"Block: {blockCount}";
+        }
+
+        for (int i = 0; i < twelveCount; i++)
+        {
+            rollNum = player.GetComponent<Player>().RollD12s();
+            blockCount += rollNum;
+            twelveRoll = true;
+            yield return new WaitForSeconds(1f);
+            twelveRoll = false;
+            dTwelveText.GetComponent<Text>().text = $"{rollNum}";
+            yield return new WaitForSeconds(0.3f);
+            blockText.GetComponent<Text>().text = $"Block: {blockCount}";
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        while (sixRoll)
+        {
+            dSix.transform.rotation = Quaternion.Slerp(dSix.transform.rotation, new Quaternion(0, 0, 360, 0), 0.2f);
+            if(dSix.transform.rotation.z >= 360)
+            {
+                dSix.transform.rotation = new Quaternion(0, 0, 0, 0);
+            }
+        }
+        dSix.transform.rotation = new Quaternion(0, 0, 0, 0);
+
+        while (eightRoll)
+        {
+            dEight.transform.rotation = Quaternion.Slerp(dEight.transform.rotation, new Quaternion(0, 0, 360, 0), 0.2f);
+            if (dEight.transform.rotation.z >= 360)
+            {
+                dEight.transform.rotation = new Quaternion(0, 0, 0, 0);
+            }
+        }
+        dEight.transform.rotation = new Quaternion(0, 0, 0, 0);
+
+        while (twelveRoll)
+        {
+            dTwelve.transform.rotation = Quaternion.Slerp(dTwelve.transform.rotation, new Quaternion(0, 0, 360, 0), 0.2f);
+            if (dTwelve.transform.rotation.z >= 360)
+            {
+                dTwelve.transform.rotation = new Quaternion(0, 0, 0, 0);
+            }
+        }
+        dTwelve.transform.rotation = new Quaternion(0, 0, 0, 0);
     }
 }
