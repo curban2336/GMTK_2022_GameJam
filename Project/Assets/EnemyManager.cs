42799c3cfef4c8bf1ec8d0bdc3a6330ba8a97084
curban2336 <curban841@gmail.com>
2022-07-17 12:58:36 -0400

    Encounter scripts

--- Project/Assets/EnemyManager.cs
+++ Project/Assets/EnemyManager.cs
@@ -6,13 +6,41 @@ public class EnemyManager : MonoBehaviour
 {
     public List<BaseCharacter> enemies;
 
+    [SerializeField] List<GameObject> enemyPos;
+
+    [SerializeField] GameObject enemy;
+    [SerializeField] GameObject e1Health;
+    [SerializeField] GameObject e2Health;
+    [SerializeField] GameObject e3Health;
+    [SerializeField] Transform pos1;
+    [SerializeField] Transform pos2;
+    [SerializeField] Transform pos3;
+
+    [SerializeField] private bool walkUpOne = false;
+    private bool walkUpTwo = false;
+
     [SerializeField] Player player;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         player.enemyList = enemies;
+        e1Health.SetActive(false);
+        e2Health.SetActive(false);
+        e3Health.SetActive(false);
     }
+
+    void Start()
+    {
+        EncounterOne();
+    }
+
+    public void EncounterOne()
+    {
+        walkUpOne = true;
+        e1Health.SetActive(true);
+    }
+
+
 
     public void TakeTurn()
     {
@@ -24,6 +52,29 @@ public class EnemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (walkUpOne)
+        {
+            if (enemyPos[0].transform.position.x >= pos1.position.x+0.01)
+            {
+                enemyPos[0].transform.position = Vector3.Lerp(enemyPos[0].transform.position, pos1.position, 1f * Time.deltaTime);
+            }
+            else
+            {
+                walkUpOne = false;
+            }
+        }
+        if (walkUpTwo)
+        {
+            if (enemyPos[1].transform.position.x >= pos2.position.x + 0.01)
+            {
+                enemyPos[1].transform.position = Vector3.Lerp(enemyPos[1].transform.position, pos1.position, 1f * Time.deltaTime);
+                enemyPos[2].transform.position = Vector3.Lerp(enemyPos[2].transform.position, pos1.position, 1.5f * Time.deltaTime);
+            }
+            else
+            {
+                walkUpTwo = false;
+            }
+            
+        }
     }
 }
